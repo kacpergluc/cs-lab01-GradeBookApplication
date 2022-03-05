057a5001ea3967996ae8219baa5819e390c9fc5f
kacpergluc <kgluc00@gmail.com>
2022-03-05 17:07:50 +0100

    Overrided RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,34 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (averageGrade >= 80)
+            {
+                return 'A';
+            }
+            else if (averageGrade < 80 && averageGrade >= 60)
+            {
+                return 'B';
+            }
+            else if (averageGrade < 60 && averageGrade >= 40)
+            {
+                return 'C';
+            }
+            else if (averageGrade < 40 && averageGrade >= 20)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
     }
 }
